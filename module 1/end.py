fce2ec1e660d6ca0d8d73cb4f8dac09fd62ee93c
ddnjcjn <sorathiyapriyanshu@gmail.com>
2022-09-15 09:50:24 +0530

    demo

---
+++ module 1/end.py
@@ -0,0 +1,9 @@
+# without end function 
+
+print("Python")
+print("Programing")
+
+# without end function 
+
+print("Python",end=" ")
+print("Programing")
---
+++ module 1/sep.py
@@ -0,0 +1,1 @@
+print("Hi","who are you?","welcome to my world!",sep="/")
